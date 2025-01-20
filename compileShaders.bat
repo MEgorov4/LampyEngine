ad26bf6191e517e3a5a10d224b640fe7029c8cdb
Mikhail <47919432+MEgorov4@users.noreply.github.com>
2025-01-20 13:24:31 +0500

    update yml

--- compileShaders.bat
+++ compileShaders.bat
@@ -1,3 +1,3 @@
 echo "COMPILE_SHADERS"
-glslc.exe Source/Shaders/shader.vert -o Source/Shaders/vert.spv
-glslc.exe Source/Shaders/shader.frag -o Source/Shaders/frag.spv
+glslc.exe Source/Shaders/shader.vert -o build/vert.spv
+glslc.exe Source/Shaders/shader.frag -o build/frag.spv
